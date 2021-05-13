b226ad6cfb0eb9096bdd0296cb44817cccd733b5
Olu Tifase <olu_tifase@hotmail.com>
2021-05-13 21:00:42 +0100

    Implemented test.

---
+++ IncentiveGamesAssessment/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IncentiveGamesAssessment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IncentiveGamesAssessment")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6145a2b-217b-4348-b8b5-7b9ea3c03919")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IncentiveGamesAssessment/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace IncentiveGamesAssessment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Creating a driver object
+            IWebDriver driver = new ChromeDriver();
+
+            // Navigating to url
+            driver.Navigate().GoToUrl("https://incentivegames.com");
+
+            // Maximizing window
+            driver.Manage().Window.Maximize();
+
+            // Clicking 'What We Do' link at the top of the page
+            driver.FindElement(By.PartialLinkText("What We")).Click();
+
+            // Close the window
+            driver.Quit();
+        }
+    }
+}
